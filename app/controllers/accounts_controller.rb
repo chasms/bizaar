3a0fdf6ce63ee858b71cf1ab3a62fe00c71126ca
chiasms <chasms@chas.ms>
2017-03-10 11:44:17 -0500

    added account model, implemented new, create, show

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,31 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show]
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params(:username, :email, :password))
+    if @account.save
+      session[:account_id] = @account.id
+      redirect_to @account
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+      def set_account
+        @account = Account.find(params[:id])
+      end
+
+      def account_params(*args)
+        params.require(:account).permit(*args)
+      end
 end
