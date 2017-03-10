99c46c1880218f2c6530e2052144c6028eb996d3
chiasms <chasms@chas.ms>
2017-03-10 15:42:01 -0500

    worked on building out session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,27 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+    redirect_to '/' unless logged_out?
   end
 
   def create
-    @account = Account.find_by()
+    @account = Account.find_by(account_params(:username))
+    return head(:forbidden) unless @account.authenticate(account_params(:password))
+    session[:user_id] = @user.id
   end
 
   def destroy
     session.clear
     redirect_to '/'
+  end
+
+private
+
+  def account_params(*args)
+    params.require(:account).permit(*args)
+  end
+
+  def logged_out?
+    session[:name] == nil
   end
 end
