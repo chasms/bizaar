e8374074d24f0bb30e0ccc7a9932dc1ae2e3755f
chiasms <chasms@chas.ms>
2017-03-10 10:54:46 -0500

    generated account model, created db, changed title of readme

---
+++ app/models/account.rb
@@ -0,0 +1,8 @@
+class Account < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :email, presence: true, uniqueness: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :username, presence: true, uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+end
