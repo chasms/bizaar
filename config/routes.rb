65bb484f23914fc623cac26359993d3594a2ca03
fakebenjay <fakebenjay@gmail.com>
2017-03-12 01:18:42 -0500

    Fixed ability to log into existing account, & login/index page redirects based on login status

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  root 'accounts#show'
+
   resources :bids
   resources :accounts do
   	resources :listings
   end
   # get '/accounts/:id/listings/:id/bids/', to: 'bids#new'
-  
-  get '/login', to: 'sessions#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
