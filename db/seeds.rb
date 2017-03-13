6486ab8c6ac68cecb382acaa4a16767c946399a1
fakebenjay <fakebenjay@gmail.com>
2017-03-13 15:04:58 -0400

    Ben added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,36 @@ Account.create([
     email: "sweetdude2@dudeness.com",
     password: "password"
   },
+  {
+    username: "Lefty",
+    email: "lefty@fail.ca",
+    password: "failure"
+  },
+  {
+    username: "Space Lord Emperor",
+    email: "altaleb@space.com",
+    password: "mondospiffy"
+  },
+  {
+    username: "Jordan",
+    email: "jordan@bass.com",
+    password: "secretguitarist"
+  },
+  {
+    username: "Talor",
+    email: "talor@guitar.com",
+    password: "secretbassist"
+  },
+  {
+    username: "Jeff",
+    email: "jeffrey@hechlertime.com",
+    password: "sotakeoffallyourclothes"
+  },
+  {
+    username: "Russell",
+    email: "russell@blacksmith.state.ma.us",
+    password: "blacksmith"
+  }
 ])
 
 Listing.create([
@@ -25,8 +55,88 @@ Listing.create([
     account_id: 1
   },
   {
-    name: "left sock",
-    description: "smells a bit funky",
+    name: "right sock",
+    description: "smells a bit country",
     account_id: 1
   },
-  ])
+  {
+    name: "middle sock",
+    description: "smells a bit rock and roll",
+    account_id: 1
+  },
+  {
+    name: "bruh",
+    description: "yeah bruh",
+    account_id: 2
+  },
+  {
+    name: "Fail",
+    description: "War is not diplomacy.",
+    account_id: 3
+  },
+  {
+    name: "Lars",
+    description: "Please god take him before he kills again.",
+    account_id: 3
+  },
+  {
+    name: "Shoehorn",
+    description: "War IS diplomacy (you know I am right.)",
+    account_id: 4
+  },
+  {
+    name: "Utilikilt",
+    description: "For utilities and weddings.",
+    account_id: 4
+  },
+  {
+    name: "Bass",
+    description: "Someday I will have a guitar.",
+    account_id: 5
+  },
+  {
+    name: "Rival Ranch",
+    description: "Talor suxxx",
+    account_id: 5
+  },
+  {
+    name: "Guitar",
+    description: "Someday I will have a bass.",
+    account_id: 6
+  },
+  {
+    name: "Leonberger",
+    description: "OMG SO CUDDLY",
+    account_id: 6
+  },
+  {
+    name: "Peter Hechler",
+    description: "We're a juggalo-themed web app. Of course you can trade siblings here.",
+    account_id: 7
+  },
+  {
+    name: "Half-eaten French Toast",
+    description: "They worked really hard on this.",
+    account_id: 7
+  },
+  {
+    name: "Happy Meal",
+    description: "Balled up.",
+    account_id: 7
+  },
+  {
+    name: "Steel",
+    description: "I forged this.",
+    account_id: 8
+  },
+  {
+    name: "Mead",
+    description: "I also forged this.",
+    account_id: 8
+  },
+  {
+    name: "Paneer",
+    description: "A sweet fat kitty.",
+    account_id: 8
+  },
+])
