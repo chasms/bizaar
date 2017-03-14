155f8934d066b847e0cbb2bbac900b471c269a51
chiasms <chasms@chas.ms>
2017-03-14 13:53:21 -0400

    formatted seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ALL ACCOUNTS
+
 Account.create([
   {
     username: "CoolUser1",
@@ -57,6 +60,8 @@ Account.create([
     password: "allahuakbar"
   },
 ])
+
+# ALL LISTINGS
 
 Listing.create([
   {
