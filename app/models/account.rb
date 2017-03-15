5c64ccde744ef9ecc8149403d8fadc7d89273863
chiasms <chasms@chas.ms>
2017-03-15 14:45:30 -0400

    fixed activerecord relationships

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,27 +1,18 @@
 class Account < ApplicationRecord
   has_many :listings
-
+  has_many :buyer_bids, through: :listings
+  has_many :seller_bids, through: :listings
   before_save { self.email = email.downcase }
   validates :email, presence: true, uniqueness: true,
     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :username, presence: true, uniqueness: true
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
-
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/avatars/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  def buyer_bids
-    # Bid.where('? = ?', Listing.find(buyer_listing_id).account_id, self.id)
-    Listing.where('account_id = ?', self.id).bids
-  end
-
-  def seller_bids
-    Bid.where('? = ?', Listing.find(seller_listing_id).account_id, self.id)
-  end
-
   def all_bids
-    buyer_bids && seller_bids
+    self.buyer_bids + self.seller_bids
   end
 
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,10 +1,6 @@
 class Bid < ApplicationRecord
-  belongs_to :seller_listing, :class_name => "Listing", :foreign_key => "buyer_listing_id"
-  belongs_to :buyer_listing, :class_name => "Listing", :foreign_key => "seller_listing_id"
-  
-  # belongs_to :seller_account, through: :seller_listing, :class_name=>"Account"
-  # belongs_to :buyer_account, through: :buyer_listing, :class_name=>"Account"
-
-
-
+  belongs_to :seller_listing, class_name: "Listing", :foreign_key => "seller_listing_id"
+  belongs_to :buyer_listing, class_name: "Listing", :foreign_key => "buyer_listing_id"
+  has_one :seller_account, through: :seller_listing, source: :account
+  has_one :buyer_account, through: :buyer_listing, source: :account
 end
