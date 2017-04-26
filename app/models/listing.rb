1b9a931dcef81d96fb71ad9b07cf157873c7b215
nanyr <nanyramirez0@gmail.com>
2017-04-26 16:46:08 -0400

    added validations to listings title and edited font size for each listing show to adjust layout on index page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
 	has_many :buyer_bids, class_name: "Bid", foreign_key: "buyer_listing_id"
 	has_many :seller_bids, class_name: "Bid", foreign_key: "seller_listing_id"
 
-	validates :name, presence: true
+	validates :name, presence: true, length:{maximum: 20}
 	validates :description, presence: true
 
 	has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/photos/:style/missing.png"
